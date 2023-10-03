1a6345fc86ba05e57effdd7ad417656a7b5dcfbb
Victor Cotoman <victorcotoman@Victors-MacBook-Pro.local>
2023-10-03 16:44:40 +0300

    Added postgress tf

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const { Client } = require('pg');
+
+const dbConfig = {
+  user: 'your-db-username', // Replace with your database username
+  host: 'your-db-host', // Replace with your RDS endpoint
+  database: 'your-db-name', // Replace with your database name
+  password: 'your-db-password', // Replace with your database password
+  port: 5432 // Replace with your database port
+};
+
+const client = new Client(dbConfig);
+client.connect();
 
 // Body parser middleware
 app.use(bodyParser.json());
-
-// Database (Example: In-memory database)
-const db = [];
 
 // GET /status
 app.get('/status', (req, res) => {
@@ -14,12 +23,25 @@ app.get('/status', (req, res) => {
 });
 
 // POST /data
-app.post('/data', (req, res) => {
-  db.push(req.body);
-  res.json({ status: 'Data received' });
+app.post('/data', async (req, res) => {
+  const data = req.body;
+
+  try {
+    const result = await client.query('INSERT INTO your_table (data) VALUES ($1)', [JSON.stringify(data)]);
+    console.log('Data inserted:', result.rows);
+    res.json({ status: 'Data received and stored in RDS' });
+  } catch (error) {
+    console.error('Error inserting data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+process.on('SIGINT', () => {
+  client.end();
+  process.exit();
 });
